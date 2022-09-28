fc7b194404babf7ad676d25c4f5054569a385995
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-09-28 17:22:18 -0300

    Create Cabelereiro.java

---
+++ Cabelereiro.java
@@ -0,0 +1,34 @@
+public class Cabelereiro {
+	private int id;
+	private String nome;
+	private String CPF;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		if(id>0) {
+			this.id = id;
+		}
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		if(nome.length()>=3) {
+			this.nome = nome;
+		}
+	}
+	
+	public String getCPF() {
+		return CPF;
+	}
+	
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	
+}
